7cd025d5b1647690da223ba394e953ce7395c968
Dmitry <borutia@mail.ru>
2020-06-30 14:08:14 +0500

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -15,7 +15,7 @@ pipeline {
                             sh "echo '${password}' | sudo -S docker stop isng"
                             sh "echo '${password}' | sudo -S docker container rm isng"
                         } catch (Exception e) {
-                            print 'container not exist, skip clean'
+                            print 'Container not exist'
                         }
                     }
                 }
@@ -40,8 +40,8 @@ pipeline {
                         passwordVariable: 'password')
                     ]) {
 
-                        sh "echo '${password}' | sudo -S docker build ${WORKSPACE}/auto -t ivan_sitnikov_nginx"
-                        sh "echo '${password}' | sudo -S docker run -d -p 0001:80 --name isng -v /home/adminci/is_mount_dir:/stat ivan_sitnikov_nginx"
+                        sh "echo '${password}' | sudo -S docker build ${WORKSPACE}/auto -t Dmitry_Bor"
+                        sh "echo '${password}' | sudo -S docker run -d -p 0001:80 --name isng -v /home/adminci/is_mount_dir:/statistics Dmitry_Bor"
                     }
                 }
             }
@@ -55,8 +55,8 @@ pipeline {
                         passwordVariable: 'password')
                     ]) {
                         
-                        sh "echo '${password}' | sudo -S docker exec -t isng bash -c 'df -h > /stat/stats.txt'"
-                        sh "echo '${password}' | sudo -S docker exec -t isng bash -c 'top -n 1 -b >> /stat/stats.txt'"
+                        sh "echo '${password}' | sudo -S docker exec -t isng bash -c 'df -h > /statistics/statistics.txt'"
+                        sh "echo '${password}' | sudo -S docker exec -t isng bash -c 'top -n 1 -b >> /statistics/statistics.txt'"
                     }
                 }
             }
